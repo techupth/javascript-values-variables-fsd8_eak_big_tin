613afa9c4b0bcf8162390065e761691de851bb88
Sanya Bochoun <76147037+sanya-bochoun@users.noreply.github.com>
2025-02-24 14:12:04 +0700

    assignment-1 completed

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
+const restaurantName = "Chef's Table by Chef James";
+const totalBillAmount = 15000;
+const totalRestaurants = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBillAmount = 5000;  
+totalRestaurants = 2500;
