a83eb522e318114a81f5b07fb4e912181b44869b
Your Name <you@example.com>
2025-02-24 14:00:41 +0700

    feat: add ex-1-2-3-4

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,9 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
 
-// Start coding here;
-
+// Start coding here
 let fahrenheit;
+
+fahrenheit = (celsius * 1.8) + 32;
+
+console.log(fahrenheit);
